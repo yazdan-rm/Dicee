79de07c17907d16c1ed991405b6dfa764c47b6e5
yazdan-rm <116789371+yazdan-rm@users.noreply.github.com>
2023-01-28 01:48:43 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+var imageNumber1 = document.querySelectorAll("img")[0];
+
+imageNumber1.setAttribute("src",randomDiceImage1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var imageNumber2 = document.querySelectorAll("img")[1];
+
+imageNumber2.setAttribute("src",randomDiceImage2);
+
+if(randomNumber1 === randomNumber2)
+  document.querySelector("h1").textContent="Draw!";
+else if(randomNumber1 > randomNumber2)
+  document.querySelector("h1").textContent="🚩Player1 wins!";
+else
+document.querySelector("h1").textContent="Player2 wins!🚩";
